43c11730ca82993bc267b49aae8206253367a8d1
Shubham277353 <golubhandari405@gmail.com>
2025-08-31 23:44:44 +0530

    feat: add playround logic and win logic

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const createPlayers = (name, marker,index) => {
-  return { name, marker, index };
+const createPlayers = (name, marker) => {
+  return { name, marker };
 };
 
-const creatGameBoard = (() => {
+const gameBoard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
 
   const getBoard = () => board;
@@ -15,21 +15,54 @@ const creatGameBoard = (() => {
   return { getBoard, setBoard };
 })();
 
-const gameController = () => {
+const gameController = (() => {
+  const player1 = createPlayers("Golu", "X");
+  const player2 = createPlayers("Star", "O");
+  let currentPlayer = player1;
+  let gameover = false ;
 
-    const player1 = createPlayers("Golu","X", 2);
-    const player2 = createPlayers("Star","O" , 3);
-    player1.creatGameBoard.setBoard(player1.index,player1.marker);
-    player2.creatGameBoard.setBoard(player2.index,player2.marker);
-  const winingCombo = [
-    (0, 1, 2),
-    (3, 4, 5),
-    (6, 7, 8),
-    (0, 3, 6),
-    (1, 4, 7),
-    (2, 5, 8),
-    (0, 4, 8),
-    (2, 4, 6),
-  ];
-};
+  const playRound = (index) => {
+    if (gameover) return ;
+    gameBoard.setBoard(index,currentPlayer.marker);
+    switchPlayer();
+    checkWinner();
+  }
+
+
+  const switchPlayer = ()=>{
+    currentPlayer =  (currentPlayer === player1) ? player2 : player1;
+  }
+
+  const checkWinner = ()=>{
+
+    const board = gameBoard.getBoard();
+    const winningCombo = [
+        [0,1,2],[3,4,5],[6,7,8],  //rows
+        [0,3,6],[1,4,7],[2,5,8], //columns
+        [0,4,8],[2,4,6]         //diagonals
+    ];
+
+    for(let combo of winningCombo){
+        const a = combo[0];
+        const b = combo[1];
+        const c = combo[2];
+
+        if(board[a]&&board[a]===board[b]&&board[b]===board[c]){
+            console.log("Winner");
+            return gameover = true;
+        }
+    }
+  }
+
+    return {playRound};
+})();
+
+gameController.playRound(0);
+gameController.playRound(5);
+gameController.playRound(2);
+gameController.playRound(6);
+gameController.playRound(1);
+gameController.playRound(2);
+
+console.log(gameBoard.getBoard());
 
